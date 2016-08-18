194b031a137b0952367157e6ee1d937ea1b03c57
Emirhan Ozlen <emirhanozlen@gmail.com>
2016-08-19 00:59:42 +0300

    Improvements on controllers/donor and new config file
    new file:   config.js
    ---------------------
    
    Project config settings used for some situations to check
    several conditions such as debug mode to print errors
    on console

--- controllers/donor.js
+++ controllers/donor.js
@@ -1,10 +1,34 @@
-var Donor       = require("../models/donor")
+var CONFIG  = require("../config")
+var Donor   = require("../models/donor")
 
 // POST /donor/
 exports.POST = function(req, res) {
     var donor = new Donor(req.body)
-    donor.save(function(err) {
-        console.log(err)
+    // Validate the posted data
+    donor.validate(function(err) {
+        // If no errors present
+        if (assert.equal(err, null)) {
+            // Try saving
+            donor.save(function(err) {
+                if (assert.equal(err, null)) {
+                    //  Success
+                    // @TODO return unique id Req#7
+                    res.sendStatus(200)
+                }
+                else {
+                    //  Unexpected error while saving
+                    if (CONFIG.DEBUG) {
+                        console.log(err)
+                    }
+                    // @TODO Improve this (maybe redirect to page? show dynamic message?)
+                    res.sendStatus(500)
+                }
+            })
+        }
+        else {
+            // Invalid data posted
+            res.sendStatus(418)
+            // @TODO Improve this (maybe redirect to page? show dynamic message?)
+        }
     })
-    res.sendStatus(200)
 }
