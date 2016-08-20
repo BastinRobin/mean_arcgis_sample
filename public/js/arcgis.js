512391ebf3b7ea7501a63a2bd0355d135d4b71f7
Emirhan Ozlen <emirhanozlen@gmail.com>
2016-08-20 06:18:48 +0300

    Points can now be shown and update on map. Nice bullet effect on this version Took me hours to figure it out. Was painful. Thanks to well documented arcgis.

--- public/js/arcgis.js
+++ public/js/arcgis.js
@@ -1,15 +1,17 @@
 require([
-    "esri/tasks/Locator",
     "esri/Map",
+    "esri/PopupTemplate",
     "esri/views/MapView",
+    "esri/Graphic",
+    "esri/geometry/Point",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/tasks/Locator",
     "esri/widgets/Search",
     "esri/geometry/support/webMercatorUtils",
     "dojo/domReady!"
-], function(Locator, Map, MapView, Search, webMercatorUtils) {
-// Set up a locator task using the world geocoding service
-var locatorTask = new Locator({
-    url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
-})
+], function(Map, PopupTemplate, MapView, Graphic, Point, SimpleMarkerSymbol,
+            Locator, Search, webMercatorUtils) {
+
 // Create the Map
 var map = new Map({
     basemap: "streets-navigation-vector"
@@ -19,7 +21,52 @@ var view = new MapView({
     container: "arcgis-map",
     map: map,
     center: [-116.3031, 43.6088],
-    zoom: 12
+    zoom: 1
+})
+
+         /**********************
+       * Create a point graphic
+       **********************/
+
+      // First create a point geometry (this is the location of the Titanic)
+      var point = new Point({
+        longitude: -49.97,
+        latitude: 41.73
+      });
+      var pointGraphic
+      setInterval(function(){
+          point.longitude -= 1;
+          console.log(point.longitude)
+          view.graphics.remove(pointGraphic)
+          pointGraphic = new Graphic({
+            geometry: point,
+            symbol: markerSymbol
+          });
+          view.graphics.add(pointGraphic)
+        },333)
+      // Create a symbol for drawing the point
+      var markerSymbol = new SimpleMarkerSymbol({
+        color: [226, 119, 40],
+        outline: { // autocasts as new SimpleLineSymbol()
+          color: [255, 255, 255],
+          width: 2
+        }
+      });
+
+      // Create a graphic and add the geometry and symbol to it
+      
+
+  // Add the line graphic to the view's GraphicsLayer
+  view.graphics.add(pointGraphic);
+
+
+
+
+
+  
+// Set up a locator task using the world geocoding service
+var locatorTask = new Locator({
+    url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
 })
 // Setup search box
 var searchWidget = new Search({
@@ -38,18 +85,18 @@ view.ui.add(searchWidget, {
  * address is printed to the popup content.
  *******************************************************************/
 view.on("click", function(evt) {
-// Get the coordinates of the click on the view
-var x = evt.mapPoint.longitude
-var y = evt.mapPoint.latitude
-var lon = Math.round(evt.mapPoint.longitude * 1000) / 1000  // x
-var lat = Math.round(evt.mapPoint.latitude * 1000) / 1000   // y
+    // Get the coordinates of the click on the view
+    var x = evt.mapPoint.longitude
+    var y = evt.mapPoint.latitude
+    var lon = Math.round(evt.mapPoint.longitude * 1000) / 1000  // x
+    var lat = Math.round(evt.mapPoint.latitude * 1000) / 1000   // y
 
-view.popup.open({
-    // Set the popup's title to the coordinates of the location
-    title: "Resolving address...",
-    location: evt.mapPoint, // Set the location of the popup to the clicked location
-    content: "Reverse geocode: [" + lon + ", " + lat + "]" + 
-             '<a href="@' + x + ':' + y + '">I can donate blood here</a>'
+    view.popup.open({
+        // Set the popup's title to the coordinates of the location
+        title: "Resolving address...",
+        location: evt.mapPoint, // Set the location of the popup to the clicked location
+        content: "Reverse geocode: [" + lon + ", " + lat + "]" + 
+                '<a href="@' + x + ':' + y + '">I can donate blood here</a>'
 })
 // Display the popup
 // Execute a reverse geocode using the clicked location
@@ -64,6 +111,21 @@ locatorTask.locationToAddress(evt.mapPoint).then(function(
     view.popup.title = "No address was found for this location<br>" + 
                        "However you can still donate on this location"
 })})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
